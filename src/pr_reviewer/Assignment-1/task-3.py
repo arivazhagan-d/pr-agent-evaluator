e03211b0c657eba7d13917e0f092f8123cc6460b
Arivazhagan <arivazhagan.devendiran@solitontech.com>
2025-07-04 11:18:52 +0530

    Implement Assignment 1

---
+++ src/pr_reviewer/Assignment-1/task-3.py
@@ -0,0 +1,25 @@
+from math import *
+import sys, os
+
+DATA_PATH = "/data"
+
+
+def bad_naming(x, y):
+    return x + y
+
+
+def compute(x: int, y: int):
+    return x * y
+
+
+def duplicated():
+    print("Repeat")
+    print("Repeat")
+
+
+def unused_func():
+    pass
+
+
+def REDUNDANT():
+    pass
